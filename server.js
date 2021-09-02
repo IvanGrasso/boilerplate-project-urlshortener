412bd3c477dacee4913acc86e8ddcae1d097c523
Ivan Grasso <grassoivan@gmail.com>
2021-09-01 21:21:10 -0300

    More rewrites

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 
 // Set up database
-mongoose.connect(process.env['DB_URI']);
+mongoose.connect(process.env['DB_URI']).then(() => {
+  console.log("Connected to database")
+}).catch((err) => {
+  console.log("Database connection error: ", err);
+})
 
 const ShortURL = mongoose.model('ShortURL', {
   id: Number,
@@ -35,6 +39,40 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/shorturl', async (req, res) => {
+  try {
+    let shortURL = await ShortURL.findOne({ originalURL: req.body.url})
+    if (!shortURL) {
+      let url = new URL(req.body.url)
+      let domain = url.hostname.replace('www.', '')
+      await lookupDNS(domain)
+      let docCount = await ShortURL.countDocuments()
+      shortURL = new ShortURL({
+        id: docCount + 1,
+        originalURL: req.body.url
+      });
+      await shortURL.save()
+    }
+    res.json({
+      original_url: shortURL.originalURL,
+      short_url: shortURL.id
+    })
+  } catch (e) {
+    res.json({ error: e.message })
+  }
+})
+
+async function lookupDNS(domain) {
+  return new Promise((resolve, reject) => {
+    dns.lookup(domain, (err, address, family) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(address);
+    });
+  });
+}
+
 app.get('/api/shorturl/:id', async function (req, res) {
   try {
    await ShortURL.findOne({ id: req.params.id }).then(shortURL => {
@@ -47,28 +85,3 @@ app.get('/api/shorturl/:id', async function (req, res) {
     res.json({ error: e.message })
   }
 });
-
-app.post('/api/shorturl', (req, res) => {
-  let url = new URL(req.body.url)
-  let domain = url.hostname.replace('www.', '')
-  dns.lookup(domain, function(err, addresses) {
-    if (err) {
-      res.json({ error: "Invalid Hostname" })
-    } else if (addresses[0] != undefined) {
-      let count = ShortURL.countDocuments({}, function(err, docCount) {
-        if (err) { return handleError(err) }       
-        const shortURL = new ShortURL({
-        id: docCount + 1,
-        originalURL: req.body.url
-      });
-        shortURL.save(function(err) {
-          if (err) { return handleError(err) }
-          res.json({
-            original_url: shortURL.originalURL,
-            short_url: shortURL.id
-          })
-        });
-      })
-    }
-  });
-})
